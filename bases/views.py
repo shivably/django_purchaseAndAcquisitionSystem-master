dfff0463d7b56e8a280ae64a736c2f16913b0cb1
Senthil <senthilprakash2020@gmail.com>
2022-03-03 23:31:41 +0530

    Dashboard fix

--- bases/views.py
+++ bases/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect, JsonResponse
 from django.views import generic
+from inventario.models import Product, Category, Subcategory, Brand
+from compras.models import AcquisitionHeader 
+from facturacion.models import BillingHeader, Customer
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
 class MixinFormInvalid:
@@ -25,6 +28,14 @@ class NoPermission(LoginRequiredMixin, PermissionRequiredMixin, MixinFormInvalid
 class Home(LoginRequiredMixin, generic.TemplateView):
     template_name = 'bases/home.html'
     login_url = 'bases:login'
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['total_product'] = Product.objects.count()
+        context['total_customer'] = Customer.objects.count()
+        context['total_purchase_invoice'] = AcquisitionHeader.objects.count()
+        context['total_sales_invoice'] = AcquisitionHeader.objects.count()
+        return context
 
 class HomeNoPermission(generic.TemplateView):
     template_name = "bases/nopermission.html"
