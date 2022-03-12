ba526ffcde45fafe827b3ca8429f7a35c24adbe5
Senthil <senthilprakash2020@gmail.com>
2022-03-12 08:57:33 +0530

    stock widget in dashboard

--- facturacion/admin.py
+++ facturacion/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-# from .models import BillingHeader, BillingDetail
+from .models import BillingHeader, BillingDetail, Product
 # Register your models here.
 
 
 # admin.site.register(BillingHeader)
 # admin.site.register(BillingDetail)
+# admin.site.register(Product)
